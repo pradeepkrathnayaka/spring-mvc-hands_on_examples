f9085540e6f6d1e5dc23c709654d9d63cdada5d5
Pradeep Rathnayaka <pradeepkrathnayaka@gmail.com>
2019-04-03 05:48:32 +0800

    Add files via upload
    Initialize project

---
+++ springmvc-quickstart-xml-config/src/main/java/com/rmpksoft/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.rmpksoft.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String homePage() {
+		return "home";
+	}
+}
